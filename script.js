f0462988f80dc15cd80c7d93689dba0f757e8b2c
Ian Tipton <ian@bronzelabs.co.uk>
2024-05-21 16:01:02 +0100

    When scrolling on the page, take the scroll position of the page into account. It previously worked fine if the scrollY position was 0 but it would otherwise be in the wrong position.

--- script.js
+++ script.js
@@ -7,23 +7,24 @@ function handleEvents(event) {
     return;
   }
 
+  // Get the position of the iframe on the page
+  const iframeY = document
+    .getElementById("tribe-training-iframe")
+    .getBoundingClientRect().top;
+  const iframeOffset = window.scrollY + iframeY;
+
   switch (event.data?.type) {
     /* Called when the the course is first loaded */
     case "ready":
-      window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({ top: iframeOffset, behavior: "smooth" });
       break;
     /* The course is requesting that we scroll the page to a certain point */
     case "scroll":
       // Scroll to this position in the iframe
       const posY = event.data.position?.top;
 
-      // Get the position of the iframe on the page
-      const iframeY = document
-        .getElementById("iframe")
-        .getBoundingClientRect().top;
-
       // Scroll to the given position, taking the position of the iframe into account
-      const scrollY = iframeY + posY;
+      const scrollY = iframeOffset + posY;
       window.scrollTo({
         top: scrollY,
         behavior: "smooth",
@@ -32,7 +33,9 @@ function handleEvents(event) {
     /* The size of the content has changed */
     case "resize":
       const height = event.data?.size?.height || 900;
-      document.getElementById("iframe").style.height = `${height}px`;
+      document.getElementById(
+        "tribe-training-iframe"
+      ).style.height = `${height}px`;
       break;
     case "close":
       window.location.href = "index.html";
